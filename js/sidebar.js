71a4423ec41588a1bdc25b91c7912f4cd759dc7d
irsandymsv <irsandymsv98@gmail.com>
2019-10-14 21:35:49 +0700

    menambahkan validasi form dan mengubah tampilan sidebar

--- js/sidebar.js
+++ js/sidebar.js
@@ -10,16 +10,16 @@ $(function() {
 	});
 
 	$("#btn_openSide").click(function(event) {
-		// $(".sidebar").width(250);
-		$(".sidebar").show();
-		// $(".main-content").css('margin-left', '250px');
-		$(this).parent("#open_sideMenu").hide();
+		$(".sidebar").css('width', '250px');
+		$(".content-right").css('margin-left', '250px');
+		// $(".sidebar").css('display', 'table-cell');
+		// $("#open_sideMenu").hide();
 	});
 
 	$("#btn_closeSide").click(function(event) {
-		// $(".sidebar").width(0);
-		$(".sidebar").hide();
-		// $(".main-content").css('margin-left', '0px');
-		$("#open_sideMenu").show();
+		$(".sidebar").css('width', '0');
+		$(".content-right").css('margin-left', '0');
+		// $(".sidebar").hide();
+		// $("#open_sideMenu").show();
 	});
 });
